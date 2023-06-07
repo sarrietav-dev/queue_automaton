a77d6c6222b993447255ce3f3a0afb5c4ad278ae
Sebastian Arrieta <25210925+sarrietav-dev@users.noreply.github.com>
2023-06-07 17:07:02 -0500

    ⚗️ Experiment with the dot language

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,31 @@
-import { Graphviz } from 'graphviz-react';
+import { Graphviz } from "graphviz-react";
 
 function App() {
+  const dot = `
+  digraph finite_state_machine {
+    rankdir=LR;
+    size="8,5"
+
+    node [shape = doublecircle]; S[color=red];
+    node [shape = point ]; qi[color=white]
+
+    node [shape = circle, color = blue]; q1;
+    node [shape = circle, color = green]; q2;
+    qi -> S;
+    S  -> q1 [ label = "a" ];
+    S  -> S  [ label = "a" ];
+    q1 -> S  [ label = "a" ];
+    q1 -> q2 [ label = "b" ];
+    q2 -> q1 [ label = "b" ];
+    q2 -> q2 [ label = "b" ];
+}
+  `;
+
   return (
     <>
-      <Graphviz dot={`digraph { a -> b }`} />
+      <Graphviz dot={dot} />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
